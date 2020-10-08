57034a39352621c32668048e58465ee2f0d78769
Jesse Newland <jesse@jnewland.com>
2020-10-08 17:55:22 -0500

    append check suite id

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,15 +12,16 @@ curl -s \
     -H "Content-Type: application/json" \
     -H "Accept: application/vnd.github.antiope-preview+json" \
     "https://api.github.com/repos/${GITHUB_REPOSITORY}/commits/${INPUT_SHA:-$SHA}/check-runs" \ |
-    jq -r '.check_runs[] | [.started_at, .completed_at, .name] | @tsv' | \
+    jq -r '.check_runs[] | [.started_at, .completed_at, .name, .check_suite.id] | @tsv' | \
     sort -n > /tmp/checkruns
 
 # Record traces for each completed check run
 while IFS="" read -r checkrun || [ -n "$checkrun" ]
 do
-    name=$(echo "$checkrun" | cut -f 3-)
-    bt_start "$name" "$(echo "$checkrun" | cut -f 1)"
-    bt_end "$name" "$(echo "$checkrun" | cut -f 2)"
+    name=$(echo "$checkrun" | cut -f 3)
+    id=$(echo "$checkrun" | cut -f 4)
+    bt_start "$name-$id" "$(echo "$checkrun" | cut -f 1)"
+    bt_end "$name-$id" "$(echo "$checkrun" | cut -f 2)"
 done < /tmp/checkruns
 
 # display the results
