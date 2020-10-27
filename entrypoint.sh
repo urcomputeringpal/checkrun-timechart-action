85f26695b520b50cffe2a2e4f1a2480b768fafb5
Jesse Newland <jesse@jnewland.com>
2020-10-27 08:31:17 -0500

    debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -26,6 +26,17 @@ curl -s \
     sort -n > /tmp/checkruns.tsv
 
 first=$(head -n 1 /tmp/checkruns.tsv | cut -f 1)
+last=$(cat /tmp/checkruns.json | \
+    jq -r '.check_runs[] | [.completed_at] | @tsv' | \
+    grep -E '[0-9]' | \
+    sort -rn | \
+    tee /tmp/completed_at.tsv | \
+    head -n 1)
+
+if [ -n "$BT_DEBUG" ]; then
+    find /tmp -name '*.tsv' -o -name '*.json' | xargs -n 1 -t cat
+fi
+
 bt_init "${INPUT_TRACE_START:-$first}"
 
 # Record traces for each completed check run
@@ -48,17 +59,9 @@ do
 done < /tmp/checkruns.tsv
 
 # display the results
-last_completed=$(cat /tmp/checkruns.json | \
-    jq -r '.check_runs[] | [.completed_at] | @tsv' | \
-    grep -E '[0-9]' | \
-    sort -rn | \
-    tee /tmp/completed_at.tsv | \
-    head -n 1)
 
 if [ -n "$INPUT_WARNING" ]; then
-    echo "::warning ::$(escapeData "$(bt_cleanup "${last_completed}")")"
+    echo "::warning ::$(escapeData "$(bt_cleanup "${last}")")"
 else
-    bt_cleanup "${last_completed}"
+    bt_cleanup "${last}"
 fi
-
-# find /tmp -name '*.tsv' -o -name '*.json' | xargs -n 1 -t cat
